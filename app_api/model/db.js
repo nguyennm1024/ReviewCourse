2b1fb73aa5c589f076558f852ee736ea64538b9d
nguyennm1024 <you@example.com>
2018-11-22 08:40:24 +0700

    created DB

--- app_api/model/db.js
+++ app_api/model/db.js
@@ -7,9 +7,9 @@ mongoose.connection.on('connected', () => console.log('connected to ' + uri));
 mongoose.connection.on('disconnected', () => console.log('disconnected to ' + uri));
 mongoose.connection.on('error', err => console.log(err));
 
-// require('./admin');
-//require('./class');
-// require('./lecturer');
-//require('./report');
-//require('./student');
+require('./admin');
+require('./class');
+require('./lecturer');
+require('./report');
+require('./student');
 require('./seed')
