2584d595dc3f463705861dcf1f9db078b244bacf
nguyennm1024 <you@example.com>
2018-12-21 13:18:58 +0700

    import generalReport to lecturerCtrl

--- app_api/controller/lecturerCtrl.js
+++ app_api/controller/lecturerCtrl.js
@@ -3,7 +3,7 @@ const Student = require('../model/student')
 const Lecturer = require('../model/lecturer')
 const Class = require('../model/class')
 const Report = require('../model/report')
-
+const GeneralReport = require('../model/reportGeneral')
 const allClass = async (req, res) => {
     const {_id} = req.body;
     let lecturerClass = await Lecturer.findOne({_id}).populate('teachingClass').exec();
